109fc80bd84fce4e7ac9e0aef029be8cacd2cfc8
ArthurHDEV <arthurhap.pro@gmail.com>
2025-12-04 10:43:37 -0300

    feat: adição do modelo LogSchema para estruturação dos logs

---
+++ src/models/Log.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+const LogSchema = new mongoose.Schema({
+  // Nível do erro (INFO, ERROR, CRITICAL)
+  level: { 
+    type: String, 
+    required: true,
+    index: true, // índice para busca rápida (Tunning)
+    uppercase: true,
+    enum: ['INFO', 'WARNING', 'ERROR', 'CRITICAL'] // Validação básica
+  },
+
+  // Serviço ou componente que gerou o log
+  service: { 
+    type: String, 
+    required: true, 
+    index: true // Cria índice para busca rápida
+  },
+
+  // Mensagem do log
+  message: { 
+    type: String, 
+    required: true 
+  },
+
+  // Dados adicionais (opcional)
+  // Usamos Mixed para permitir qualquer estrutura de dados
+  metadata: {
+    type: mongoose.Schema.Types.Mixed,
+    default: {}
+  },
+
+  // Data do evento
+  timestamp: {
+    type: Date,
+    default: Date.now,
+    index: true // Importante para ordenação
+  }
+}, {
+  // Configurações extras do Mongoose
+  timestamps: true // Cria createdAt e updatedAt automaticamente
+});
+
+// Índice TTL para expiração automática dos logs após 30 dias (2592000 segundos)
+LogSchema.index({ timestamp: 1 }, { expireAfterSeconds: 2592000 });
+
+export default mongoose.model('Log', LogSchema);
